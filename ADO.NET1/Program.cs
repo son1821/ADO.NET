4edbb2be9c83c3b860e47c5c6d47d27871e54fe1
Ngoc Son Ninh <123456nuct@gmail.com>
2024-01-14 13:08:30 +0700

    Add project files.

---
+++ ADO.NET1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Data.Common;
+using System.Data.SqlClient;
+
+namespace ADO.NET1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sqlStringBuiler = new SqlConnectionStringBuilder();
+            sqlStringBuiler["Server"] = "localhost,1433";
+            sqlStringBuiler["Database"] = "xtlab";
+            sqlStringBuiler["UID"] = "sa";
+            sqlStringBuiler["PWD"] = "Password123";
+            var sqlStringConnection = sqlStringBuiler.ToString();
+            Console.WriteLine(sqlStringConnection);
+
+            //string sqlStringConnection = "Data Source=localhost,1433;Initial Catalog =xtlab;User ID=sa;Password=Password123";
+            using var connection = new SqlConnection(sqlStringConnection);
+            Console.WriteLine(connection.State);
+            connection.Open();
+            Console.WriteLine(connection.State);
+            //truy van..
+            using DbCommand command = new SqlCommand();
+            command.Connection =   connection;
+            command.CommandText = "SELECT TOP (10) * FROM Sanpham";
+            var datareader = command.ExecuteReader();
+            while (datareader.Read())
+            {
+                Console.WriteLine($"{datareader["TenSanpham"], 10}, Gia {datareader["Gia"], 8}");
+            }
+            connection.Close();
+        }
+    }
+}
